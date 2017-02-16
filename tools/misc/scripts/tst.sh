70663fc2f7f8253d05563a237e4609be07557ef6
Adam Scott <scottadam23@gmail.com>
2017-02-16 13:33:17 -0500

    Thu 16 Feb 2017 13:33:16 EST Cleaned up the scripts folder a bit...

--- tools/misc/scripts/tst.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-var=`find ${1} -maxdepth 1 -type f -printf '%s %p'| sort -nr | head -1`
-echo ${var} >> ~/tst.txt
-var=`expr match "${var}" ' .*'`
-# cp ${var} "${var}.tst"
-echo ${var} >> ~/tst.txt
